f9dba8e782e79b3dc981918a49af9d0cf773d0e0
JeremyConnor <ultra31magnus@gmail.com>
2021-05-29 03:46:28 +0530

    Added Marketplace/Homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: "DePharm",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-theme-material-ui",
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,72 @@
-import * as React from "react";
+// import * as React from "react";
+// import Layout from '../components/layout';
 
-const IndexPage = () => {
+// const IndexPage = () => {
+//     return (
+//         <Layout pageTitle="Home Page">
+//             <p>This webpage is a step to create DePharm</p>
+//         </Layout>
+//     )
+// }
+
+// export default IndexPage;
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Vaccine from '../components/vaccine-card.js';
+import Equipments from '../components/equipments-card.js';
+import Medecine from '../components/medecine-card.js';
+
+// const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    // backgroundColor: 'white',
+    backgroundColor: '#16161a',
+    height: "100vh",
+  },
+  // necessary for content to be below app bar
+//   toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    // [theme.breakpoints.up('sm')]: {
+    //   width: `calc(100% - ${drawerWidth}px)`,
+    //   marginLeft: drawerWidth,
+    // },
+  },
+}));
+
+const IndexPage = (props) => {
+  const { window } = props;
+  const classes = useStyles();
+  // const theme = useTheme();
+
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to DePharm</h1>
-    </main>
-  )
+    <div className={classes.root}>
+      <main className={classes.content}>
+        <div>
+          <Grid 
+            container 
+            spacing={0}
+            alignItems = "center"
+            justify = "center"
+          >
+            <Vaccine />
+            <Medecine />
+            <Equipments />
+          </Grid>
+        </div>
+      </main>
+    </div>
+  );
 }
+
+IndexPage.propTypes = {
+  window: PropTypes.func,
+};
 
 export default IndexPage;
